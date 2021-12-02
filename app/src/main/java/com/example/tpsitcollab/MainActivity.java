1b4379dffead56491e8353cb8567dec21efecfc4
FrancescoBasso <frnacescobasso03@gmail.com>
2021-12-02 08:38:17 +0100

    messaggio

--- app/src/main/java/com/example/tpsitcollab/MainActivity.java
+++ app/src/main/java/com/example/tpsitcollab/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.tpsitcollab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button Play;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Play= (Button) findViewById(R.id.play);
+
+
+    }
+
+    public void test(View v)
+    {
+        Toast t = Toast.makeText(getApplicationContext(),"Francesco", Toast.LENGTH_LONG);// tre parametri(dove viene invocato,messaggio visualizzato,durata)
+        // oggetto TOAST ha anche una gravity per posizionarlo
+        t.show();
     }
 }
